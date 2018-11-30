92268a5153565327a73313a3a3f0e75b568d739e
Jeroen Heijmans <github@jeroenheijmans.nl>
2018-11-30 22:52:25 +0100

    Prepare day 01

---
+++ AdventOfCode2018/Day01.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Xunit;
+
+namespace AdventOfCode2018
+{
+    public class Day01
+    {
+        [Theory]
+        [InlineData(@"", "0")]
+        public void Test_Solve1(string input, object expected)
+        {
+            Assert.Equal(expected, Solve1(input));
+        }
+
+        [Theory]
+        [InlineData(@"", "0")]
+        public void Test_Solve2(string input, object expected)
+        {
+            Assert.Equal(expected, Solve2(input));
+        }
+
+        public object Solve1(string input)
+        {
+
+
+            return 0;
+        }
+
+        public object Solve2(string input)
+        {
+
+
+            return 0;
+        }
+    }
+}

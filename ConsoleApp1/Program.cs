9b30abbb4b5e453c3c88cafb4ec6d3de07cf0b0f
Jeroen Heijmans <github@jeroenheijmans.nl>
2018-12-04 06:46:28 +0100

    Add day 04 solutions

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,9 +4,24 @@ namespace ConsoleApp1
 {
     class Program
     {
+        public const string puzzleInput = "";
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine(Solve1(puzzleInput));
+
+            Console.ReadKey();
+        }
+
+        public static long Solve1(string input)
+        {
+            var data = input.Split(",");
+
+            var result = -1L;
+
+            return result;
         }
     }
 }

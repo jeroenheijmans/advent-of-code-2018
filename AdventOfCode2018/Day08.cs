820c6e7ece9a9406ad66256c58724a19e2fcc760
Jeroen Heijmans <github@jeroenheijmans.nl>
2018-12-07 18:25:51 +0100

    Prepare day 08

---
+++ AdventOfCode2018/Day08.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using Xunit;
+using static AdventOfCode2018.Util;
+
+namespace AdventOfCode2018
+{
+    public class Day08
+    {
+        public const string puzzleInput = "";
+
+        [Theory]
+        [InlineData(0L, "")]
+        [InlineData(-1L, puzzleInput)]
+        public void Test_Solve1(long expected, string input)
+        {
+            Assert.Equal(expected, Solve1(input));
+        }
+
+        [Theory]
+        [InlineData(0L, "")]
+        [InlineData(-1L, puzzleInput)]
+        public void Test_Solve2(long expected, string input)
+        {
+            Assert.Equal(expected, Solve2(input));
+        }
+
+        public long Solve1(string input)
+        {
+            var data = input.Split(",");
+            return 0;
+        }
+
+        public long Solve2(string input)
+        {
+            var data = input.Split(",");
+            return 0;
+        }
+    }
+}

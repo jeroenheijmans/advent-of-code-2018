73456376d33cd39e9dceb32ee44034d2175eee2f
Jeroen Heijmans <github@jeroenheijmans.nl>
2018-12-08 06:44:01 +0100

    Prepare day 09

---
+++ AdventOfCode2018/Day09.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using Xunit;
+using static AdventOfCode2018.Util;
+
+namespace AdventOfCode2018
+{
+    public class Day09
+    {
+        public const string testInput = "";
+        public const string puzzleInput = "";
+
+        [Fact] public void Solution_1_test_example() => Assert.Equal(0, Solve1(testInput));
+        [Fact] public void Solution_1_test_real_input() => Assert.Equal(0, Solve1(puzzleInput));
+
+        [Fact] public void Solution_2_test_example() => Assert.Equal(0, Solve2(testInput));
+        [Fact] public void Solution_2_test_real_input() => Assert.Equal(0, Solve2(puzzleInput));
+        
+        public int Solve1(string input)
+        {
+            var data = input.Split(",");
+
+            return -1;
+        }
+
+        public int Solve2(string input)
+        {
+            var data = input.Split(" ");
+
+            return -1;
+        }
+    }
+}

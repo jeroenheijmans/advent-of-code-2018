f4dbe371fe1b60ee12eb542f9275f41ede18f4bb
Jeroen Heijmans <github@jeroenheijmans.nl>
2018-12-11 22:04:25 +0100

    Start with Infi AoC puzzle 2

--- AdventOfCode2018/Infi.Aoc.nl.cs
+++ AdventOfCode2018/Infi.Aoc.nl.cs
@@ -33,13 +33,13 @@ namespace AdventOfCode2018
 ╠╝╗╩╚╗╦╦╚╣╗╠║╦╔╗╚╗═╬";
 
         [Fact] public void Solve_puzzle_1() => Assert.Equal(40, Solve1(puzzleInput));
+        [Fact] public void Solve_puzzle_2() => Assert.Equal(0, Solve2(puzzleInput));
 
-        [DebuggerDisplay("{Char} ({X},{Y})")]
         public class Node
         {
-            public int X { get; set; } // For debugging only
-            public int Y { get; set; } // For debugging only
-            public char Char { get; set; } // For debugging only
+            public int X { get; set; }
+            public int Y { get; set; }
+            public char Char { get; set; }
             public ISet<Node> ConntectedNodes { get; } = new HashSet<Node>();
 
             public void ConnectTo(Node other)
@@ -56,19 +56,44 @@ namespace AdventOfCode2018
 
         public int Solve1(string input)
         {
-            var grid = input.Split(new string[] { "\n", "\r", "\r\n" }, StringSplitOptions.None)
-                .Where(s => !string.IsNullOrWhiteSpace(s))
-                .Select(row => row.ToArray())
-                .ToArray();
-
-            Node[,] nodes = ConvertToNodeArray(grid);
-            var height = grid.Length;
-            var width = grid.First().Length;
+            Node[,] nodes = ConvertToNodeArray(input);
             var current = nodes[0, 0];
-            var goal = nodes[width - 1, height - 1];
+            var goal = nodes[nodes.GetLength(0) - 1, nodes.GetLength(1) - 1];
             var visitedNodes = new HashSet<Node> { current };
 
             return GetShortestDistanceTo(current, goal, visitedNodes);
+        }
+
+        public int Solve2(string input)
+        {
+            Node[,] nodes = ConvertToNodeArray(input);
+            return GetShortestTwilightDistanceTo(0, 0, 0, nodes);
+        }
+
+        private int GetShortestTwilightDistanceTo(int currentX, int currentY, int counter, Node[,] grid)
+        {
+            if (currentX == grid.GetLength(0) - 1 && currentY == grid.GetLength(1))
+            {
+                return 0;
+            }
+
+            if (counter > 100)
+            {
+                return int.MaxValue; // Escape hatch for now, maxrecursion (sort of)
+            }
+
+            int shortestPath = int.MaxValue - 1; // Same trick as puzzle 1
+
+            foreach (var target in grid[currentX, currentY].ConntectedNodes)
+            {
+                var clone = Clone(grid);
+                // shift!! and as part of the shift: reconnect nodes properly
+                // possibly shift Santa here as well?
+                var dist = GetShortestTwilightDistanceTo(target.X, target.Y, counter + 1, clone);
+                shortestPath = Math.Min(shortestPath, dist);
+            }
+
+            return shortestPath;
         }
 
         private int GetShortestDistanceTo(Node current, Node goal, ISet<Node> visitedNodes)
@@ -93,8 +118,42 @@ namespace AdventOfCode2018
             return shortestPath;
         }
 
-        private static Node[,] ConvertToNodeArray(char[][] grid)
+        private static Node[,] Clone(Node[,] grid)
         {
+            var nodes = new Node[grid.GetLength(0), grid.GetLength(1)];
+
+            for (int y = 0; y < grid.GetLength(1); y++)
+            {
+                for (int x = 0; x < grid.GetLength(0); x++)
+                {
+                    nodes[x, y] = new Node { X = x, Y = y, Char = grid[x, y].Char };
+
+                    if (x > 0
+                        && nodes[x, y].IsOpenLeft
+                        && nodes[x - 1, y].IsOpenRight)
+                    {
+                        nodes[x, y].ConnectTo(nodes[x - 1, y]);
+                    }
+
+                    if (y > 0
+                        && nodes[x, y].IsOpenTop
+                        && nodes[x, y - 1].IsOpenBottom)
+                    {
+                        nodes[x, y].ConnectTo(nodes[x, y - 1]);
+                    }
+                }
+            }
+
+            return nodes;
+        }
+
+        private static Node[,] ConvertToNodeArray(string input)
+        {
+            var grid = input.Split(new string[] { "\n", "\r", "\r\n" }, StringSplitOptions.None)
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Select(row => row.ToArray())
+                .ToArray();
+
             var height = grid.Length;
             var width = grid.First().Length;
 

8bb6cf2746e523ba3d2660a3add08117892d3d55
Jeroen Heijmans <github@jeroenheijmans.nl>
2018-12-11 06:27:34 +0100

    Add day 11 puzzle 1 (wrong) attempt

--- AdventOfCode2018/Day11.cs
+++ AdventOfCode2018/Day11.cs
@@ -12,25 +12,81 @@ namespace AdventOfCode2018
 {
     public class Day11
     {
-        public const string testInput = @"";
-        public const string puzzleInput = @"";
+        public const long puzzleInput = 6878;
 
-        [Fact] public void Solution_1_test_example() => Assert.Equal(0, Solve1(testInput));
+        public const int gridSize = 300;
+
+        [Fact] public void Solution_1_test_example_1() => Assert.Equal(29, Solve1(18));
+        [Fact] public void Solution_1_test_example_2() => Assert.Equal(30, Solve1(42));
         [Fact] public void Solution_1_test_real_input() => Assert.Equal(0, Solve1(puzzleInput));
 
-        [Fact] public void Solution_2_test_example() => Assert.Equal(0, Solve2(testInput));
-        [Fact] public void Solution_2_test_real_input() => Assert.Equal(0, Solve2(puzzleInput));
+        [Fact] public void Solution_2_test_example() => Assert.Equal(0, Solve2(0));
+        [Fact] public void Solution_2_test_real_input() => Assert.Equal(0, Solve2(0));
 
-        public int Solve1(string input)
+        public long Solve1(long serialNr)
         {
-            var data = input.Split(",");
+            Dictionary<Point, long> energyLevels = GetEnergyLevelsGrid(serialNr);
 
-            return -1;
+            var sums = new Dictionary<Point, long>();
+
+            for (int y = 1; y <= gridSize - 3; y++)
+            {
+                for (int x = 1; x <= gridSize - 3; x++)
+                {
+                    long newSum = 0;
+
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            newSum += energyLevels[new Point(x + i, y + j)];
+                        }
+                    }
+
+                    sums.Add(new Point(x, y), newSum);
+                }
+            }
+
+            // Not 30
+            return sums.Select(x => x.Value).Max();
         }
 
-        public int Solve2(string input)
+        private static Dictionary<Point, long> GetEnergyLevelsGrid(long serialNr)
         {
-            var data = input.Split(" ");
+            var energyLevels = new Dictionary<Point, long>();
+
+            for (int y = 1; y <= gridSize; y++)
+            {
+                for (int x = 1; x <= gridSize; x++)
+                {
+                    long level = GetLevelFor(serialNr, y, x);
+
+                    energyLevels.Add(new Point(x, y), level);
+                }
+            }
+
+            return energyLevels;
+        }
+
+        [Fact] public void GetLevelFor_given_example1_should_return_result() => Assert.Equal(4, GetLevelFor(8, y: 5, x: 3));
+        [Fact] public void GetLevelFor_given_example2_should_return_result() => Assert.Equal(-5, GetLevelFor(57, y: 79, x: 122));
+        [Fact] public void GetLevelFor_given_example3_should_return_result() => Assert.Equal(0, GetLevelFor(39, y: 196, x: 217));
+        [Fact] public void GetLevelFor_given_example4_should_return_result() => Assert.Equal(4, GetLevelFor(71, y: 153, x: 101));
+
+        private static long GetLevelFor(long serialNr, int y, int x)
+        {
+            long rackId = x + 10;
+            long levelStart = rackId * y;
+            long withSerial = levelStart + serialNr;
+            long multiplied = withSerial * rackId;
+            long hundreth = (multiplied / 100) % 10;
+            long level = hundreth - 5;
+
+            return level;
+        }
+
+        public long Solve2(long serialNr)
+        {
 
             return -1;
         }

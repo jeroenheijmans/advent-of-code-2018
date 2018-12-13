014b0dfe92e1d3eb17463ef1f439075aa10c126b
Jeroen Heijmans <github@jeroenheijmans.nl>
2018-12-13 20:07:50 +0100

    Prepare day 14

---
+++ AdventOfCode2018/Day14.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using Xunit;
+using Xunit.Abstractions;
+using static AdventOfCode2018.Util;
+
+namespace AdventOfCode2018
+{
+    public class Day14
+    {
+        private readonly ITestOutputHelper output;
+
+        public Day14(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        public string exampleInput = @"";
+        public string puzzleInput = @"";
+
+        [Fact] public void Solution_1_test_example() => Assert.Equal("", Solve1(exampleInput));
+        [Fact] public void Solution_1_test_real_input() => Assert.Equal("", Solve1(puzzleInput));
+
+        [Fact] public void Solution_2_test_example() => Assert.Equal("", Solve2(exampleInput));
+        [Fact] public void Solution_2_test_real_input() => Assert.Equal("", Solve2(puzzleInput));
+
+        public string Solve1(string input)
+        {
+            return "";
+        }
+
+        public string Solve2(string input)
+        {
+            return "";
+        }
+    }
+}

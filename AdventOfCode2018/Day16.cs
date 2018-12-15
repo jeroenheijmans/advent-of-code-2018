94c6ef7198fbd5336dcdd0f950a9572536ffbb30
Jeroen Heijmans <github@jeroenheijmans.nl>
2018-12-15 22:48:40 +0100

    Prepare day 16

---
+++ AdventOfCode2018/Day16.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using Xunit;
+using Xunit.Abstractions;
+using static AdventOfCode2018.Util;
+
+namespace AdventOfCode2018
+{
+    public class Day16
+    {
+        private readonly ITestOutputHelper output;
+
+        public Day16(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        public const string testInput = "";
+        public const string puzzleInput = "";
+
+        [Fact] public void Solution_1_test_example() => Assert.Equal(0, Solve1(testInput));
+        [Fact] public void Solution_1_test_real_input() => Assert.Equal(0, Solve1(puzzleInput));
+
+        [Fact] public void Solution_2_test_example() => Assert.Equal(0, Solve2(testInput));
+        [Fact] public void Solution_2_test_real_input() => Assert.Equal(0, Solve2(puzzleInput));
+
+        public int Solve1(string input)
+        {
+            var data = input.Split(",");
+
+            return -1;
+        }
+
+        public int Solve2(string input)
+        {
+            var data = input.Split(" ");
+
+            return -1;
+        }
+    }
+}

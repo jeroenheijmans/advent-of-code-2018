201948d2dcd0c5949876cda26337de5ab9ba1dd2
Jeroen Heijmans <github@jeroenheijmans.nl>
2018-12-18 06:29:16 +0100

    Add day 18 puzzle 1 solution

---
+++ AdventOfCode2018/Day18.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using Xunit;
+using Xunit.Abstractions;
+using static AdventOfCode2018.Util;
+
+namespace AdventOfCode2018
+{
+    public class Day18
+    {
+        private readonly ITestOutputHelper output;
+
+        public Day18(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        public const string testInput = @"
+.#.#...|#.
+.....#|##|
+.|..|...#.
+..|#.....#
+#.#|||#|#|
+...#.||...
+.|....|...
+||...#|.#|
+|.||||..|.
+...#.|..|.
+";
+
+        public const string puzzleInput = @"
+.#....|###.|.||...|..|#|..#...###|...|....#....##.
+#.|.|.||.#...|.#..#.#.##.##||.###|..|.#.|.#...|.#|
+..|#...#|||......#..#.|..#.|..#|##....|||.....##..
+..#|.|......|.##...#.|||.|.||.#.|.#...#...#.....|.
+####|.##.|#...|..||#.#.##....#|..|.#|...#...#.....
+.#.#.|....##...#..|.|..|.#.|...#..#|#....|#|#..#..
+||...|#.....#..............#..#|..##.|#|#|.#.#....
+.#|##...|.|.|....#|....#|.|.|...#|..#.......#.||#|
+....#.|..#..###|#|.#.#......#.#.#..#.#.#....#.#|||
+#.#.#..#...|....#####|....#...|#|...#|#.|.....||..
+.#...#|....||..||..|....##.#..#..#.|#.....|.#|.##.
+....|..|..#|..#||.|#.....||..#|.|............#...|
+..#...|#...|#|.....|....#...##....#.|#...#..##|##.
+........|.#.#.#.|#..|..#...|...|#||.##.|........|.
+..#.##...#.#.##|.||.|#..#|.|.#|.|.|...#|.|..|.|.|.
+##....|#.|##|...|##..#.||..|..#.|....#.##||.......
+.....|...|...|||...#.|##||||.|.####.||#.##|#.#...#
+..|##|.#..#.|.||..|...#...|........#.|.|...##.|.#.
+.##.#|.|.|..||....#.|..#|.|..#|.||#||##.....#.|.|.
+.#...||.|..............|......#|..##.#.#|.||.#..#.
+.....|#.|##|.||##.#..###..#.##..|..|....|...#.#||.
+|........|.|##..#||.#.|.|.##.|.....#.......||....#
+|##|.#.|...#.#||#.|.||..#|#.....|.|.....##..#..||.
+....||..|.|....||..|#..|.#.|.##..|...##..........|
+.#.#|#.#|..|..|.||#...|......|..#|#.#..#.###......
+|.|...|##.|#..##....|#|....##|..#|#|.|..#...|#|...
+....||....###......|.#.|#.|#.#|...#.|.##.......|.#
+...|.....|..#.|.|.|#.|###|||...|.#||#..#......#..#
+...##|......#..#|#|...||||.|##|.#.......#|....##..
+|.|||#.#..|...#..|#.|...|..#.|.##..#.#........#.|#
+|##.......|.#|##|...|....#|.#.|.#.#||.#|.#...##.|#
+##.#.#.|#......#...#|.....||.|..||#|...|#..||#....
+..|#..#.....#.#......|.||#..||....#...#|##...##..|
+.|..|.|.||..|##..#.###.||#.|...#...|.#.......|.|#.
+..#|...##.##.......|#..##|...|.|.#|#|...|.#..#..|.
+.#|#|.||.|||...##|.#..|...#..##|#|.#..#......##|.|
+..|.....#|..#....|....#.........##...|.....|...#.#
+..|#.|..#.###.||||.||||#.|.||#....#.........|.|...
+...#.|..#..|..||....|...#....|.#|##.#...|#|.#||...
+....|.|......####|.#..|........||.#|.#|.#....#..|#
+...||....|||..|....|..|...##.|.|#...|..|.|..|##.#.
+.|.####...#.#|...###.|.|......|......#....#..#....
+....#.###.....#|..||..##||.|||....|###.###||...||.
+.|.|..#..#|..#.#.##..|.#.|..#.#..##..#||...|..|.#.
+.|..|.#.###.|.#.||.##.........#.#|#...##..#.|#...|
+##|.|.##..||.##|...#|##..|#.|...||..|.#..#.|.#|.|.
+.|#.#|..|.#|.||.#.##.##...||.....#|.....|..|#.....
+#|.##.##.|##.#.#|.....|.##||.|#.#.#.#..#...||..#||
+...||...|#...##..||.....|..|....|.#.|.#......#.|..
+.|...|.||.||.|...|#|......|....|..|#.#.......#...|
+";
+
+        [Fact] public void Solution_1_test_example() => Assert.Equal(1147, Solve1(testInput));
+        [Fact] public void Solution_1_test_real_input() => Assert.Equal(-1, Solve1(puzzleInput));
+
+        //[Fact] public void Solution_2_test_example() => Assert.Equal(0, Solve2(testInput));
+        //[Fact] public void Solution_2_test_real_input() => Assert.Equal(0, Solve2(puzzleInput));
+
+        public int Solve1(string input)
+        {
+            var data = input.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None)
+                .Select(l => l.Trim())
+                .Where(l => !string.IsNullOrEmpty(l))
+                .Select(l => l.ToArray())
+                .ToArray();
+
+            var width = data[0].Length;
+            var height = data.Length;
+
+            var grid1 = new char[width, height];
+            var grid2 = new char[width, height];
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    grid1[x, y] = data[y][x];
+                    grid2[x, y] = data[y][x];
+                }
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                OutputGrid(width, height, grid1);
+
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        int treecount = 0;
+                        int yardcount = 0;
+
+                        for (int n = x - 1; n < x + 2; n++)
+                        {
+                            for (int m = y - 1; m < y + 2; m++)
+                            {
+                                if (n < 0 || n >= width) continue;
+                                if (m < 0 || m >= height) continue;
+                                if (n == x && y == m) continue;
+
+                                if (grid1[n, m] == '|') treecount++;
+                                if (grid1[n, m] == '#') yardcount++;
+                            }
+                        }
+
+                        if (grid1[x, y] == '.') {
+                            if (treecount >= 3) grid2[x, y] = '|';
+                        }
+
+                        if (grid1[x, y] == '|') {
+                            if (yardcount >= 3) grid2[x, y] = '#';
+                        }
+
+                        if (grid1[x, y] == '#') {
+                            if (yardcount >= 1 && treecount >= 1) grid2[x, y] = '#';
+                            else grid2[x, y] = '.';
+                        }
+                    }
+                }
+
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        grid1[x, y] = grid2[x, y];
+                    }
+                }
+            }
+
+            var totalTrees = 0;
+            var totalYards = 0;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (grid1[x, y] == '|') totalTrees++;
+                    if (grid1[x, y] == '#') totalYards++;
+                }
+            }
+
+            return totalTrees * totalYards;
+        }
+
+        private void OutputGrid(int width, int height, char[,] grid1)
+        {
+            output.WriteLine("");
+
+            for (int y = 0; y < height; y++)
+            {
+                var sb = new StringBuilder();
+                for (int x = 0; x < width; x++)
+                {
+                    sb.Append(grid1[x, y]);
+                }
+                output.WriteLine(sb.ToString());
+            }
+            output.WriteLine("");
+        }
+
+        public int Solve2(string input)
+        {
+            var data = input.Split(" ");
+
+            return -1;
+        }
+    }
+}

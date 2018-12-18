b6f45508c553392a53496c7e4db53c308d1381f3
Jeroen Heijmans <github@jeroenheijmans.nl>
2018-12-18 21:43:56 +0100

    Prepare day 19

---
+++ AdventOfCode2018/Day19.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+using static AdventOfCode2018.Util;
+
+namespace AdventOfCode2018
+{
+    public class Day19
+    {
+        private readonly ITestOutputHelper output;
+
+        public Day19(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        public const string testInput = @"";
+
+        public const string puzzleInput = @"";
+
+        [Fact] public void Solution_1_test_example() => Assert.Equal(-1, Solve1(testInput));
+        [Fact] public void Solution_1_test_real_input() => Assert.Equal(-1, Solve1(puzzleInput));
+
+        public int Solve1(string input)
+        {
+            var data = input.SplitByNewline(shouldTrim: true);
+            return 0;
+        }
+    }
+}

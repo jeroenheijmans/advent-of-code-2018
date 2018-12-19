697437379aba0c790f6d537baa58e94a752a9c68
Jeroen Heijmans <github@jeroenheijmans.nl>
2018-12-19 17:48:26 +0100

    Prepare day 20

---
+++ AdventOfCode2018/Day20.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+using static AdventOfCode2018.Util;
+
+namespace AdventOfCode2018
+{
+    public class Day20
+    {
+        private readonly ITestOutputHelper output;
+
+        public Day20(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        public const string testInput = @"";
+
+        public const string puzzleInput = @"";
+
+        [Fact] public void Solution_1_test_example() => Assert.Equal(-1, Solve1(testInput));
+        [Fact] public void Solution_1_test_real_input() => Assert.Equal(-1, Solve1(puzzleInput));
+
+        public int Solve1(string input)
+        {
+            return 0;
+        }
+    }
+}

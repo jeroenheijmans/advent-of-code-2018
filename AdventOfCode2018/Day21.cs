aa83386c23eaebb1e914882b4c8c59dcaad0c7c2
Jeroen Heijmans <github@jeroenheijmans.nl>
2018-12-21 16:06:42 +0100

    Start with hand rolled day 21 puzzle 1

--- AdventOfCode2018/Day21.cs
+++ AdventOfCode2018/Day21.cs
@@ -56,108 +56,47 @@ seti 5 9 2
         [Fact] public void Solution_1_test_real_input() => Assert.Equal(-1, Solve1(puzzleInput));
 
         public int Solve1(string input)
-        {
-            var initForRegister0 = 0;
-
-            var reg = new[] { initForRegister0, 0, 0, 0, 0, 123 };
-
-        one:
-            reg[5] = reg[5] & 456;
-            if (reg[5] == 72) reg[5] = 0;
-            else goto one;
-
-        six:
-            reg[4] = reg[5] | 65536;
-            reg[5] = 15466939;
-
-        eight:
-            reg[3] = reg[4] & 255;
-            reg[5] = reg[5] + reg[3];
-            reg[5] = reg[5] & 16777215;
-            reg[5] = reg[5] * 65899;
-            reg[5] = reg[5] & 16777215;
-
-            if (256 > reg[4]) goto twentyeight;
-            else goto seventeen;
-
-        seventeen:
-            reg[3] = 0;
-
-        eighteen:
-            reg[1] = (reg[3] + 1) * 256;
-
-            if (reg[1] > reg[4]) goto twentysix;
-            else goto twentyfour;
-
-        twentyfour:
-            reg[3]++;
-            goto eighteen;
-
-        twentysix:
-            reg[4] = reg[3];
-            goto eight;
-
-        twentyeight:
-            if (reg[5] == reg[0])
-            {
-                throw new Exception("Program halts, but we have no clue anymore how many instructions it was");
-            }
-            else
-            {
-                goto six;
-            }
-
-            throw new NoSolutionFoundException();
-        } 
-
-        public int Solve1BruteForce(string input)
-        {
-            var data = input.SplitByNewline(shouldTrim: true);
-
-            var ipRegister = int.Parse(data.First().Replace("#ip ", ""));
-
-            var program = data.Skip(1)
-                .Select(line => line.Split())
-                .Select(line =>
-                {
-                    var inst = new int[4];
-                    inst[0] = ElfCodeMachine.OpCodesByName[line[0]];
-                    inst[1] = int.Parse(line[1]);
-                    inst[2] = int.Parse(line[2]);
-                    inst[3] = int.Parse(line[3]);
-                    return inst;
-                })
-                .ToArray();
-
-            for (int i = 0; i < 10_000; i++)
-            {
-                var result = SolveInternal(program, ipRegister, initForRegister0: i);
-                if (result >= 0) return i;
-            }
-
+        {            
             throw new NoSolutionFoundException();
         }
 
-        private int SolveInternal(int[][] program, int ipRegister, int initForRegister0, int maxExecutions = 1_000)
+        private int HandrolledProgram(int initForRegister0)
         {
-            var ip = 0;
-            var registers = new[] { initForRegister0, 0, 0, 0, 0, 0 };
-            var counter = 0;
+            var reg = new[] { initForRegister0, 0, 0, 0, 0, 0 };
 
-            while (ip < program.Length)
-            {
-                registers[ipRegister] = ip;
-                ElfCodeMachine.Doop(program[ip], registers);
-                ip = registers[ipRegister];
+            //  0:  reg[5] = 123
+            //  1:  reg[5] = reg[5] & 456
+            //  2:  reg[5] = (reg[5] == 72) ? 1 : 0
+            //  3:  ip = 3 + reg[5]
+            //  4:  ip = 0
+            //  5:  reg[5] = 0
+            //  6:  reg[4] = reg[5] | 65536
+            //  7:  reg[5] = 15466939
+            //  8:  reg[3] = reg[4] & 255
+            //  9:  reg[5] = reg[5] + reg[3]
+            // 10:  reg[5] = reg[5] & 16777215
+            // 11:  reg[5] = reg[5] * 65899
+            // 12:  reg[5] = reg[5] & 16777215
+            // 13:  reg[3] = (256 > reg[4]) ? 1 : 0
+            // 14:  ip = 14 + reg[3]
+            // 15:  ip = 16
+            // 16:  ip = 27
+            // 17:  reg[3] = 0
+            // 18:  reg[1] = reg[3] + 1
+            // 19:  reg[1] = reg[1] * 256
+            // 20:  reg[1] = (reg[1] > reg[4]) ? 1 : 0
+            // 21:  ip = 21 + reg[1]
+            // 22:  ip = 23
+            // 23:  ip = 25
+            // 24:  reg[3]++
+            // 25:  ip = 17
+            // 26:  reg[4] = reg[3]
+            // 27:  ip = 7
+            // 28:  reg[3] = (reg[5] == reg[0]) ? 1 : 0
+            // 29:  ip = 29 + reg[3]
+            // 30:  ip = 5
 
-                ip++;
-
-                if (counter++ > maxExecutions) return -1;
-            }
-
-            output.WriteLine($"REGISTERS after {counter} executions: {string.Join(";", registers)}");
-
-            return registers[0];
+            return initForRegister0;
         }
     }
 }

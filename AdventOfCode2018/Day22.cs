c8a5a183d136a17f824d76a8f6530bcaa8ac715b
Jeroen Heijmans <github@jeroenheijmans.nl>
2018-12-22 00:20:52 +0100

    Prepare day 22

---
+++ AdventOfCode2018/Day22.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+using static AdventOfCode2018.Util;
+
+namespace AdventOfCode2018
+{
+    public class Day22
+    {
+        private readonly ITestOutputHelper output;
+
+        public Day22(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        public const string testInput = @"";
+
+        public const string puzzleInput = @"";
+
+        [Fact] public void Solution_1_test_example_1() => Assert.Equal(-1, Solve1(testInput));
+
+        [Fact] public void Solution_1_test_real_input() => Assert.Equal(-1, Solve1(puzzleInput));
+
+        public int Solve1(string input)
+        {
+            return 0;
+        }
+    }
+}

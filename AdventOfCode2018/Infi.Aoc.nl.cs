b9a6fa1b0d1967029719964f3a8fff07827e910e
Jeroen Heijmans <github@jeroenheijmans.nl>
2018-12-26 12:42:24 +0100

    Add new attempt at Infi AoC puzzle 2

--- AdventOfCode2018/Infi.Aoc.nl.cs
+++ AdventOfCode2018/Infi.Aoc.nl.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using Xunit;
 
 namespace AdventOfCode2018
@@ -48,11 +49,16 @@ namespace AdventOfCode2018
                 this.ConntectedNodes.Add(other);
             }
 
-            public bool IsOpenLeft => new [] { '╗', '╦', '╝', '╬', '╩', '═', '╣' }.Contains(Char);
-            public bool IsOpenRight => new [] { '╔', '╠', '╦', '╚', '╬', '╩', '═' }.Contains(Char);
-            public bool IsOpenTop => new [] { '║', '╠', '╚', '╝', '╬', '╩', '╣' }.Contains(Char);
-            public bool IsOpenBottom => new [] { '║', '╔', '╗', '╠', '╦', '╬', '╣' }.Contains(Char);
+            public bool IsOpenLeft => IsOpenLeft(Char);
+            public bool IsOpenRight => IsOpenRight(Char);
+            public bool IsOpenTop => IsOpenTop(Char);
+            public bool IsOpenBottom => IsOpenBottom(Char);
         }
+
+        public static bool IsOpenLeft(char Char) => new[] { '╗', '╦', '╝', '╬', '╩', '═', '╣' }.Contains(Char);
+        public static bool IsOpenRight(char Char) => new[] { '╔', '╠', '╦', '╚', '╬', '╩', '═' }.Contains(Char);
+        public static bool IsOpenTop(char Char) => new[] { '║', '╠', '╚', '╝', '╬', '╩', '╣' }.Contains(Char);
+        public static bool IsOpenBottom(char Char) => new[] { '║', '╔', '╗', '╠', '╦', '╬', '╣' }.Contains(Char);
 
         public int Solve1(string input)
         {
@@ -66,67 +72,168 @@ namespace AdventOfCode2018
 
         public int Solve2(string input)
         {
-            Node[,] nodes = ConvertToNodeArray(input);
-            return -1; // return GetShortestTwilightDistanceTo(0, 0, 0, nodes);
+            int size = GetGridSize(input);
+
+            var origin = new Point(0, 0);
+            var goal = new Point(size - 1, size - 1);
+            var position = origin;
+
+            var state = Regex.Replace(input, "\r?\n", "");
+            var time = 0;
+
+            var visited = new HashSet<(string state, Point position)>();
+            var edges = new HashSet<(string state, Point position, int distance)> { (state, position, 0) };
+
+            var i = 0;
+
+            while (edges.Any() && i++ < 10_000)
+            {
+                var newEdges = new HashSet<(string state, Point position, int distance)>();
+                var newstate = Shift(state, size, time);
+                time = (time + 1) % size;
+
+                foreach (var edge in edges)
+                {
+                    if (visited.Contains((edge.state, edge.position))) continue;
+                    if (edge.position == goal) return edge.distance;
+
+                    visited.Add((edge.state, edge.position));
+
+                    for (int dir = 0; dir < 4; dir++)
+                    {
+                        if (CanMoveInDirection(edge.state, edge.position, size, dir))
+                        {
+                            var newpos = GetTarget(edge.position, dir);
+                            newpos = ShiftSantaIfNeeded(newpos, time, size);
+                            newEdges.Add((newstate, newpos, edge.distance + 1));
+                        }
+                    }
+                }
+
+                edges = newEdges;
+            }
+
+            throw new NoSolutionFoundException();
         }
 
-        private int GetShortestTwilightDistanceTo(int currentX, int currentY, int counter, Node[,] grid)
+        private static int GetGridSize(string input)
         {
-            if (currentX == grid.GetLength(0) - 1 && currentY == grid.GetLength(1))
-            {
-                return 0;
-            }
+            var width = input.SplitByNewline(shouldTrim: true).First().Length;
+            var height = input.SplitByNewline(shouldTrim: true).Count();
 
-            if (counter > 40)
-            {
-                return int.MaxValue; // Escape hatch for now, maxrecursion (sort of)
-            }
+            if (width != height) throw new NotSupportedException("Only square grids are supported");
 
-            int shortestPath = int.MaxValue - 1; // Same trick as puzzle 1
-
-            foreach (var target in grid[currentX, currentY].ConntectedNodes)
-            {
-                var clone = Clone(grid);
-                var targetX = target.X;
-                var targetY = target.Y;
-
-                if (counter % 2 == 0)
-                {
-                    // Shift a row
-                    var yToShift = counter % clone.GetLength(1);
-                    var firstNode = clone[0, yToShift];
-                    for (int x = 1; x < clone.GetLength(0); x++)
-                    {
-                        clone[x, yToShift] = clone[x - 1, yToShift];
-                    }
-                    clone[clone.GetLength(0) - 1, yToShift] = firstNode;
-
-                    // shift santa
-                    if (targetY == yToShift) targetX = (targetX + 1) % clone.GetLength(0);
-                }
-                else
-                {
-                    // Shift a column
-                    var xToShift = counter % grid.GetLength(0);
-                    var firstNode = clone[xToShift, 0];
-                    for (int y = 1; y < clone.GetLength(1); y++)
-                    {
-                        clone[xToShift, y] = clone[xToShift, y - 1];
-                    }
-                    clone[xToShift, clone.GetLength(1) - 1] = firstNode;
-
-                    // shift santa
-                    if (targetX == xToShift) targetY = (targetY + 1) % clone.GetLength(1);
-                }
-
-                ReconnectNodes(clone, targetX, targetY);
-
-                var dist = GetShortestTwilightDistanceTo(targetX, targetY, counter + 1, clone);
-                shortestPath = Math.Min(shortestPath, dist);
-            }
-
-            return shortestPath;
+            var size = width;
+            return size;
         }
+
+        [Fact] public void Shift_sample_1() => Assert.Equal("4123abcd4567qwer", Shift("1234abcd4567qwer", 4, 0));
+        [Fact] public void Shift_sample_2() => Assert.Equal("1w34a2cd4b67q5er", Shift("1234abcd4567qwer", 4, 1));
+
+        private static string Shift(string state, int size, int time)
+        {
+            if (time % 2 == 0) // row
+            {
+                var sb = new StringBuilder();
+                for (int y = 0; y < size; y++)
+                {
+                    var line = state.Substring(y * size, size);
+                    if (y == time)
+                    {
+                        sb.Append(line.Last());
+                        sb.Append(line.Take(line.Length - 1).ToArray());
+                    }
+                    else
+                    {
+                        sb.Append(line);
+                    }
+                }
+                return sb.ToString();
+            }
+            else // column
+            {
+                var sb = new StringBuilder();
+                char delayedX = state[(size - 1) * size + time];
+                for (int y = 0; y < size; y++)
+                {
+                    for (int x = 0; x < size; x++)
+                    {
+                        if (x == time)
+                        {
+                            sb.Append(delayedX);
+                            delayedX = state[y * size + x];
+                        }
+                        else
+                        {
+                            sb.Append(state[y * size + x]);
+                        }
+                    }
+                }
+                return sb.ToString();
+            }
+        }
+
+        [Fact] public void ShiftSanta_sample_1() => Assert.Equal(new Point(0, 0), ShiftSantaIfNeeded(new Point(0, 0), 5, 20));
+        [Fact] public void ShiftSanta_sample_2() => Assert.Equal(new Point(2, 0), ShiftSantaIfNeeded(new Point(1, 0), 0, 20));
+        [Fact] public void ShiftSanta_sample_3() => Assert.Equal(new Point(0, 0), ShiftSantaIfNeeded(new Point(0, 0), 1, 20));
+        [Fact] public void ShiftSanta_sample_4() => Assert.Equal(new Point(1, 1), ShiftSantaIfNeeded(new Point(1, 0), 1, 20));
+
+        private static Point ShiftSantaIfNeeded(Point current, int time, int size)
+        {
+            if (time % 2 == 0 && time == current.Y)
+            {
+                return current.X == (size - 1) ? new Point(0, current.Y) : new Point(current.X + 1, current.Y);
+            }
+            else if (time % 2 == 1 && time == current.X)
+            {
+                return current.Y == (size - 1) ? new Point(current.X, 0) : new Point(current.X, current.Y + 1);
+            }
+            else
+            {
+                return current;
+            }
+        }
+
+        private static Point GetTarget(Point current, int direction)
+        {
+            if (direction == 0) return new Point(current.X, current.Y - 1);
+            if (direction == 1) return new Point(current.X + 1, current.Y);
+            if (direction == 2) return new Point(current.X, current.Y + 1);
+            if (direction == 3) return new Point(current.X - 1, current.Y);
+            throw new ArgumentException();
+        }
+
+        private static bool CanMoveInDirection(string state, Point position, int size, int direction)
+        {
+            char current = state[position.Y * size + position.X];
+
+            switch (direction)
+            {
+                case 0: // UP
+                    if (position.Y <= 0) return false;
+                    if (!IsOpenTop(current)) return false;
+                    if (!IsOpenBottom(state[(position.Y - 1) * size + position.X])) return false;
+                    return true;
+                case 1: // RIGHT
+                    if (position.X >= size - 1) return false;
+                    if (!IsOpenRight(current)) return false;
+                    if (!IsOpenLeft(state[position.Y * size + position.X + 1])) return false;
+                    return true;
+                case 2: // DOWN
+                    if (position.Y >= size - 1) return false;
+                    if (!IsOpenBottom(current)) return false;
+                    if (!IsOpenTop(state[(position.Y + 1) * size + position.X])) return false;
+                    return true;
+                case 3: // LEFT
+                    if (position.X <= 0) return false;
+                    if (!IsOpenLeft(current)) return false;
+                    if (!IsOpenRight(state[position.Y * size + position.X - 1])) return false;
+                    return true;
+                default:
+                    throw new ArgumentException();
+            }
+        }
+
 
         private int GetShortestDistanceTo(Node current, Node goal, ISet<Node> visitedNodes)
         {
